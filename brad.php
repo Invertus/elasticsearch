6dd1b50ef5ce9927aa0156f6725f01f0336f5b26
Šarūnas Jonušas <jonusas.sarunas@gmail.com>
2016-11-03 10:31:50 +0200

    added index.php files & licenses

--- brad.php
+++ brad.php
@@ -1,4 +1,21 @@
 <?php
+/**
+ * Copyright (c) 2016-2017 Invertus, JSC
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction,
+ * including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so,
+ * subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+ * INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+ * IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+ * TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
 
 if (!defined('_PS_VERSION_')) {
     exit;
@@ -41,7 +58,7 @@ class Brad extends Module
         parent::__construct();
 
         $this->displayName = $this->l('BRAD');
-        $this->description = $this->l('Elasticsearch® module for PrestaShop that makes search and filter significantly faster');
+        $this->description = $this->l('Elasticsearch® module for PrestaShop that makes search and filter significantly faster.');
         $this->ps_versions_compliancy = ['min' => '1.6.1.0', 'max' => '1.6.2.0'];
 
         $this->container = \Invertus\Brad\Container\Container::build($this);
